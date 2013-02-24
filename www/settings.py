6eb1c00adf508c1c309ca177f534a98e810b19f0
Muhammad M Rahman <mmrs151@gmail.com>
2013-02-24 23:20:07 +0000

    Simplification of settings file

--- www/settings.py
+++ www/settings.py
@@ -1,39 +1,177 @@
 import os
 
-# Import default settings for the project
-from conf.default import *
+location = lambda x: os.path.join(os.path.dirname(os.path.realpath(__file__)), x)
 
-# We use an environmental variable to indicate the, erm, environment
-# This block writes all settings to the local namespace
-module_path = os.environ.get('DJANGO_CONF', 'conf.local')
-try:
-    module = __import__(module_path, globals(), locals(), ['*'])
-except ImportError:
-    import sys
-    print "You need to create a file conf/local.py to contain your local settings"
-    sys.exit(1)
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
-for k in dir(module):
-    if not k.startswith("__"):
-        locals()[k] = getattr(module, k)
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
 
-# Add any additional apps only required locally
-if 'EXTRA_APPS' in locals():
-    INSTALLED_APPS = INSTALLED_APPS + EXTRA_APPS
+MANAGERS = ADMINS
 
-# Adjust settings based on the DISABLED_APPS setting
-if 'DISABLED_APPS' in locals():
-    INSTALLED_APPS = [k for k in INSTALLED_APPS if k not in DISABLED_APPS]
-    MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
-    TEMPLATE_CONTEXT_PROCESSORS = list(TEMPLATE_CONTEXT_PROCESSORS)
-    for app_name in DISABLED_APPS:
-        for x, m in enumerate(MIDDLEWARE_CLASSES):
-            if m.startswith(app_name):
-                MIDDLEWARE_CLASSES.pop(x)
-        for x, m in enumerate(TEMPLATE_CONTEXT_PROCESSORS):
-            if m.startswith(app_name):
-                TEMPLATE_CONTEXT_PROCESSORS.pop(x)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
-# Keep version number here - this is generally overwritten as
-# part of deployment to be the build name
-VERSION = 'UNVERSIONED'
+#################### appointment settings #################################
+ABSOLUTE_URI = 'http://127.0.0.1:8000'  # IMPORTANT FOR EMAIL CONTENT
+FROM_EMAIL = 'mmrs151@gmail.com'  # SET ACCORDING TO YOU SMTP SETTINGS
+APPOINTMENT_START = 8  # BETWEEN 0 - 24
+APPOINTMENT_END = 17  # BETWEEN 0 - 24
+APPOINTMENT_INTERVAL = 15  # IN MINUTES
+MAX_APPOINTMENT_PER_SLOT = 1  # HOW MANY YOU CAN SERVE IN EACH SLOT
+
+###########################################################################
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mmrs151@gmail.com'
+EMAIL_HOST_PASSWORD = 'mrnmrSr64'
+
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'Europe/London'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-gb'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = True
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = location('public/media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = location('public/static/')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    location('static/'),
+)
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'ek$!+f$!xfe%&amp;9ka7npjzw$t-l*p-fs99h1!=^2928+-by88-k'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+    location('templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'appointment',
+)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
